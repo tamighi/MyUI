e0fe67d59545735c7e3d502f5c0eb3bf25d2bd96
tamighi <tamighi@hotmail.com>
2023-07-18 18:04:27 +0200

    Declaration file configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: "src/lib",
+      exclude: ["**/*.stories.ts", "**/*.stories.tsx"],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib/main.ts"),
